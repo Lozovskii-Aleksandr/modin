a319f7525cc9516a435e5409b1b76137a7983bea
aleksandr.lozovskii <aleksandr.lozovskii@intel.com>
2022-01-25 18:35:02 +0300

    Added statistics to fix #4041 Added methods for the module - getting data on all `passed` and `failed`
    Signed-off-by: aleksandr.lozovskii <aleksandr.lozovskii@intel.com>

--- test_analytics/ParseOutputPytest.py
+++ test_analytics/ParseOutputPytest.py
@@ -4,7 +4,7 @@ import os
 
 class ParseOutputPytest:
     def _path_to_name(self, path_test: str):
-        """Using the saved test path, we generate a name, 
+        """Using the saved test path, we generate a name,
             replacing '\\' wand'/' with '_'"""
         text = path_test
         text = text.replace('/', '_')
@@ -14,12 +14,12 @@ class ParseOutputPytest:
         return text
 
     def _now_date(self):
-        """Get current date and time in the format: 
+        """Get current date and time in the format:
             {day}-{month}-{year}_{hour}-{minute}-{second}"""
 
         from datetime import datetime
 
-        current_datetime = datetime.now() 
+        current_datetime = datetime.now()
         year = current_datetime.year
         month = current_datetime.month
         day = current_datetime.day
@@ -32,8 +32,8 @@ class ParseOutputPytest:
         return date
 
     def _pretty_save_file(self, file_name: str):
-        """The output of pytest occurs in one line, which is not readable. 
-           Using this function, we will create a new file in which json will 
+        """The output of pytest occurs in one line, which is not readable.
+           Using this function, we will create a new file in which json will
             be written with all the indents."""
 
         with open(file_name) as f:
@@ -49,7 +49,7 @@ class ParseOutputPytest:
 
         list_line_traceback = full_error.split('\n')
 
-        #### ---- If you need another metric, then you can register it here 
+        #### ---- If you need another metric, then you can register it here
 
         # If there is a remote traceback, then we will consider only it
         slice_index = 0
@@ -79,23 +79,23 @@ class ParseOutputPytest:
         return short_error
 
     def _get_dict_name_short_error(self, dict_name_full_error: dict, length_tb: int) -> dict:
-        """Accepts a dictionary and the number `length_tb` is also accepted. 
+        """Accepts a dictionary and the number `length_tb` is also accepted.
             From the full text of the error, only the last `length_tb` lines
-            of the Traceback are left. If there are fewer lines in the 
-            error, the entire error will be returned. If `length_tb` == 0, 
-            the entire traceback text will be returned (only local or only 
-            remote). If `length_tb` == 1, only the error itself will be 
-            returned. 
+            of the Traceback are left. If there are fewer lines in the
+            error, the entire error will be returned. If `length_tb` == 0,
+            the entire traceback text will be returned (only local or only
+            remote). If `length_tb` == 1, only the error itself will be
+            returned.
 
         Args:
-            dict_name_full_error (dict): dictionary, where the 
+            dict_name_full_error (dict): dictionary, where the
                 key - test_name, and the value - full text of the error.
 
-            length_tb (int): The number shows how many last Traceback 
+            length_tb (int): The number shows how many last Traceback
                 lines to leave.
 
         Returns:
-            dict: Return dict, where key - name test, value - short text 
+            dict: Return dict, where key - name test, value - short text
                 of the error.
         """
         dict_name_short_error = {}
@@ -106,7 +106,7 @@ class ParseOutputPytest:
         return dict_name_short_error
 
     def _get_name_without_args(self, full_name: str) -> str:
-        """Gets the input name of the test, which has arguments passed 
+        """Gets the input name of the test, which has arguments passed
             in square brackets. Removes curly brackets and their contents.
 
         Args:
@@ -122,15 +122,15 @@ class ParseOutputPytest:
         return new_name
 
     def _get_group_test_without_args(self, list_test: list) -> list:
-        """For each name without parameters, we form a list of tests 
+        """For each name without parameters, we form a list of tests
             that begins with this name.
 
         Args:
             list_test (list): A list with names test with parameters.
 
         Returns:
-            list: Dictionary, where the key is the name of the test 
-                without parameters, and the value is a list of tests 
+            list: Dictionary, where the key is the name of the test
+                without parameters, and the value is a list of tests
                 whose name begins with the name of the key.
         """
         group_by_short_name = {}
@@ -143,32 +143,32 @@ class ParseOutputPytest:
 
         return group_by_short_name
 
-    def start_test(self, 
-                   path_dir_for_start: str, 
+    def start_test(self,
+                   path_dir_for_start: str,
                    path_test: str,
-                   path_dir_for_save='test_analytics\\json_analytics_from_test', 
-                   environment_variables='', 
+                   path_dir_for_save='test_analytics\\json_analytics_from_test',
+                   environment_variables='',
                    args_pytest='',
                    new_name=None) -> str:
-        """Let's run the specified test with the passed environment variables 
-            and pytest arguments. The result will be saved in json format in 
-            the folder `/test_analytics/json_analytics_from_test`. The 
-            console displays the location of the file and its name as a 
+        """Let's run the specified test with the passed environment variables
+            and pytest arguments. The result will be saved in json format in
+            the folder `/test_analytics/json_analytics_from_test`. The
+            console displays the location of the file and its name as a
             result of the program.
 
         Args:
-            path_dir_for_start (str): The path to the folder from where we will 
+            path_dir_for_start (str): The path to the folder from where we will
                 run the test.
 
             path_test (str): The path to the test that we will run.
 
             path_dir_for_save (str, optional): Path where will save json file.
 
-            environment_variables (str, optional): Environment variables. 
-                If there are several of them, then we specify the next 
+            environment_variables (str, optional): Environment variables.
+                If there are several of them, then we specify the next
                 variable  using ' && '. Defaults to None.
 
-            args_pytest (str, optional): Flags for pytest. The '--tb=...' flag 
+            args_pytest (str, optional): Flags for pytest. The '--tb=...' flag
                 cannot be specified. Defaults to None.
 
             new_name (str, optional): File name for json.
@@ -207,17 +207,17 @@ class ParseOutputPytest:
         return path_file_name
 
     def get_dict_name_with_error_text(self, name_file_with_analytics_from_test: str) -> dict:
-        """The input is a file that is obtained as a result of the 
-            `start_test` method. 
+        """The input is a file that is obtained as a result of the
+            `start_test` method.
 
         Args:
-            name_file_with_analytics_for_test (str): the name of the json 
+            name_file_with_analytics_for_test (str): the name of the json
                 file where the statistics for the test are located.
 
         Returns:
-            dict: The dictionary is returned, where the key is the name of 
-                the test, and the value is traceback (if there is a remote 
-                one, then we will write it, if there is no such one, then we 
+            dict: The dictionary is returned, where the key is the name of
+                the test, and the value is traceback (if there is a remote
+                one, then we will write it, if there is no such one, then we
                 will write a local one).
         """
         with open(name_file_with_analytics_from_test) as f:
@@ -235,18 +235,49 @@ class ParseOutputPytest:
 
         return dict_test_name_error
 
+    def get_dict_name_with_results(self, name_file_with_analytics_from_test: str) -> dict:
+        """The input is a file that is obtained as a result of the
+            `start_test` method.
+
+        Args:
+            name_file_with_analytics_for_test (str): the name of the json
+                file where the statistics for the test are located.
+
+        Returns:
+            dict: The dictionary is returned, where the key is the name of
+                the test, and the value is traceback (if there is a remote
+                one, then we will write it, if there is no such one, then we
+                will write a local one).
+        """
+        with open(name_file_with_analytics_from_test) as f:
+            data = json.load(f)
+
+        list_tests = data['report']['tests']
+        dict_test_name_result = {}
+        for test in list_tests:
+            if test["outcome"] == "failed":
+                # Get the output of pytest for a specific test
+                result = test['call']['longrepr']
+                test_name = 'failed - ' + test['name']
+            elif test["outcome"] == "passed":
+                result = "passed"
+                test_name = 'passed - ' + test['name']
+
+            dict_test_name_result[test_name] = result
+        return dict_test_name_result
+
     def get_dict_error_with_list_test(self, dict_name_full_error: dict, length_tb: int) -> dict:
-        """We call the `_get_dict_name_short_error ' method, and then we 
+        """We call the `_get_dict_name_short_error ' method, and then we
             perform grouping by mistake.
 
         Args:
-            dict_name_full_error (dict): dictionary, where the key - test_name, 
+            dict_name_full_error (dict): dictionary, where the key - test_name,
                 and the value - full text of the error.
-            length_tb (int): The number shows how many last Traceback 
+            length_tb (int): The number shows how many last Traceback
                 lines to leave.
 
         Returns:
-            dict: Return dict, where key - error, and values - list of tests 
+            dict: Return dict, where key - error, and values - list of tests
                 where this error occurs
         """
 
@@ -261,15 +292,15 @@ class ParseOutputPytest:
         return group_errors
 
     def get_dict_test_duration(self, name_file_with_analytics_from_test: str) -> dict:
-        """The input is the name of the file that is obtained as a result of 
+        """The input is the name of the file that is obtained as a result of
             the `start_test` method.
 
         Args:
-            name_file_with_analytics_from_test (str): the name of the json 
+            name_file_with_analytics_from_test (str): the name of the json
                 file where the statistics for the test are located.
 
         Returns:
-            dict: a dictionary where the key - name of the test, and the 
+            dict: a dictionary where the key - name of the test, and the
                 value - duration of the test
         """
         with open(name_file_with_analytics_from_test) as f:
@@ -288,18 +319,18 @@ class ParseOutputPytest:
         return dict_name_duration
 
     def get_group_test_with_less_time(self, path_file: str, max_duration_file: float):
-        """The input is json, from which only those tests are selected that 
-            pass if you drop the arguments in square brackets. After that,  
-            only those tests that are executed faster than max_duration_file 
+        """The input is json, from which only those tests are selected that
+            pass if you drop the arguments in square brackets. After that,
+            only those tests that are executed faster than max_duration_file
             seconds are selected from these tests.
 
         Args:
-            path_file (str): The path to the json file that is obtained as a 
+            path_file (str): The path to the json file that is obtained as a
                 result of the start_test command
             max_duration_file (float): Maximum duration of a test group
 
         Returns:
-            list: A list of tests, all of which pass and run in a time less 
+            list: A list of tests, all of which pass and run in a time less
                 than max_duration_file
         """
         with open(path_file) as f:
@@ -327,7 +358,7 @@ class ParseOutputPytest:
         # We get groups of tests without parameters (for non-failed tests)
         group_by_short_name_not_failed_test = self._get_group_test_without_args(not_failed_list_test.keys())
 
-        # We get a dictionary where the key is the name of the test 
+        # We get a dictionary where the key is the name of the test
         #   without a parameter, and the value is the duration of this group
         passed_group_test_with_sum_duration = {}
         for short_name, test_list in group_by_short_name_passed_test.items():
@@ -359,16 +390,16 @@ class ParseOutputPytest:
         return sorted(tmp)
 
     def print_top_most_frequent_errors(self, res_dict: dict, number_line: int, count_test_print=5):
-        """The input is a dictionary, from which another dictionary is 
-            grouped, where the key is the truncated part of the error, 
-            and the value is a list of errors in which the truncated part 
-            of the error matches. Then the sorting is performed and the 5 
+        """The input is a dictionary, from which another dictionary is
+            grouped, where the key is the truncated part of the error,
+            and the value is a list of errors in which the truncated part
+            of the error matches. Then the sorting is performed and the 5
             most common errors are displayed.
 
         Args:
-            res_dict (dict): A dictionary where the key is the name of the 
+            res_dict (dict): A dictionary where the key is the name of the
                 test, and the value is the full text of the error.
-            number_line (int): The number shows how many last Traceback 
+            number_line (int): The number shows how many last Traceback
                 lines to leave.
             count_test_print (int): We will output so many errors to the console
         """
@@ -380,11 +411,11 @@ class ParseOutputPytest:
 
         sorted_list_error = sorted(list_error_count_test, key=lambda x: x[1], reverse=True)
 
-        # If the number of tests in the array is less than this number, 
+        # If the number of tests in the array is less than this number,
         #   then we will output all the tests
-        number_tests_print = 5 
+        number_tests_print = 5
 
-        # If the number of tests is greater than the previous number, 
+        # If the number of tests is greater than the previous number,
         #   then we will output the following number of tests
         number_tests_print_part = 5
 
@@ -397,10 +428,10 @@ class ParseOutputPytest:
 
             if len(list_test) < number_tests_print:
                 for test in list_test:
-                    print('    ', test)
+                    print('* ', test)
             else:
                 for test in list_test[:number_tests_print_part]:
-                    print('    ', test)
+                    print('* ', test)
 
                 print(f'And also in {len(list_test) - number_tests_print_part} tests.')
 
